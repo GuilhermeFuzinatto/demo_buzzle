549bda447f4c67d9f62b0d38c0ac7abc1f1e300c
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-05-22 10:33:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ let currentIndex = 0;
 function moveSlide(step) {
     const slides = document.querySelectorAll('.divcarrossel');
     const totalSlides = slides.length;
-    currentIndex = (currentIndex + step + totalSlides) % totalSlides;
-    const carrossel = document.querySelector('.divcarrossel');
-    carrossel.style.transform = `translateX(-${currentIndex * 100}%)`;
+    
+    // Verifica se o carrossel não ultrapassou o limite
+    if (currentIndex + step >= totalSlides || currentIndex + step < 0) {
+        return;  // Não faz nada se o limite for atingido
+    }
+    
+    // Se o índice for válido, atualiza a posição
+    currentIndex = currentIndex + step;
+
+    // Atualiza o transform do carrossel para mostrar o slide correspondente
+    const carrossel = document.querySelector('.seccarrossel');
+    carrossel.style.transform = `translateX(-${currentIndex * 33.33}%)`;
 }
