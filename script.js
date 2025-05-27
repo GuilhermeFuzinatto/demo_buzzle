966c3b17f33a876bd0c527568806828fe5d2ef50
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-05-27 16:29:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,8 +14,65 @@ function anialun(){
 }
 
 //VISUALIZAÇÃO DE SENHA CADASTRO
+const button1 = document.getElementById('botao1');
+let botao1click = false;
+const button2 = document.getElementById('botao2');
+let botao2click = false;
+
+button1.addEventListener('click', function() {
+    if (botao1click == false){
+        botao1click = true;
+    }else{
+        botao1click = false;
+    }
+});
+
+button2.addEventListener('click', function() {
+    if (botao2click == false){
+        botao2click = true;
+    }else{
+        botao2click = false;
+    }
+});
+
 function anibotao1(){
-    
+    const btn = document.getElementById('botao1');
+    const zoi = document.getElementById('olho1');
+    const inp = document.getElementById('senha1');
+    // Remove a classe anterior
+    btn.classList.remove('versenha', 'saisenha');
+
+    if(!botao1click){
+    btn.classList.add('versenha');
+    zoi.src = 'imagens/vesim.png';
+    inp.type = 'text';
+    botao1click = true;
+    }else{
+    btn.classList.add('saisenha');
+    zoi.src = 'imagens/venao.png';
+    inp.type = 'password';
+    botao1click = false;
+    }
+}
+
+function anibotao2(){
+    const btn = document.getElementById('botao2');
+    const zoi = document.getElementById('olho2');
+    const inp = document.getElementById('senha2');
+    // Remove a classe anterior
+    btn.classList.remove('versenha', 'saisenha');
+
+    if(!botao2click){
+    btn.classList.add('versenha');
+    zoi.src = 'imagens/vesim.png';
+    inp.type = 'text';
+    botao2click = true;
+    }else{
+    btn.classList.add('saisenha');
+    zoi.src = 'imagens/venao.png';
+    inp.type = 'password';
+    botao2click = false;
+    }
 }
 
 //CARROSSEL HOMEPAGE
