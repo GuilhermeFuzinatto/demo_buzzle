f39165a2560945f09592d49ad8e7405dbdc4e709
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-08-07 08:43:48 -0300

    Add files via upload

---
+++ public/cadturma.js
@@ -0,0 +1,41 @@
+//GERAR ID 
+function gerarId() {
+    let id = '';
+    for (let i = 0; i < 6; i++) {
+        const rand = Math.floor(Math.random() * 36); // número de 0 a 35
+        id += rand.toString(36); // converte para caractere base36
+    }
+    return id;
+}
+
+//CADASTRO DE TURMA
+async function cadastrarTurma(event) {
+    event.preventDefault();
+
+    const turma = {
+        id: gerarId(),
+        nome: document.getElementById("nome").value,
+        desc: document.getElementById("desc").value
+    };
+
+    try {
+        const response = await fetch('/clientes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(cliente)
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            alert("Cliente cadastrado com sucesso!");
+            document.getElementById("cliente-form").reset();
+        } else {
+            alert(`Erro: ${result.message}`);
+        }
+    } catch (err) {
+        console.error("Erro na solicitação:", err);
+        alert("Erro ao cadastrar cliente.");
+    }
+}
